e1259a22bd064ee011e230f2ab64d8ca72f204d7
Camille <camillebujotzek02@gmail.com>
2026-02-07 18:04:39 +0100

    Init Vite React TS + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-green-400 flex items-center justify-center font-mono">
+      <h1 className="text-4xl">Hacker</h1>
+    </div>
+  );
+}
